c4fd5c139c8623ea5ef55c664b6f070f83f3c633
Alex Wibowo <alexwibowo@gmail.com>
2019-06-12 05:08:21 +1000

    Removing obsolete code and images

--- src/com/wibowo/intellij/mdd/MarioIcons.java
+++ src/com/wibowo/intellij/mdd/MarioIcons.java
@@ -11,8 +11,4 @@ public interface MarioIcons {
 
     Icon MARIO_STANDING_FROM_RIGHT =  IconLoader.getIcon("/mario_standing_from_right_transparent_small.png");
     Icon MARIO_RUNNING_FROM_RIGHT =  IconLoader.getIcon("/mario_running_from_right_transparent_small.png");
-
-
-    Icon MARIO_ICON = IconLoader.getIcon("/mario_from_left.png");
-    Icon RMARIO_ICON = IconLoader.getIcon("/mario_from_right.png");
 }
